99711e09f32855f1bc1ec1a2d0516d22aacad113
Chel <80759997+TheVictimZ@users.noreply.github.com>
2024-02-24 16:00:58 +0700

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,73 @@
+import { createRequire } from 'module'
+const require = createRequire(import.meta.url)
+const version = require("@adiwajshing/baileys/package.json").version 
+global.require = require
+global.language = "id"
+global.nomerOwner = "62877424664779"// Ganti sesuka luh
+global.nomerOwner2 = "62877424664779"// Ganti sesuka luh
+global.runWith = "Samsung Galaxy A70" // Ganti sesuka luh
+global.ownerName = "Im Chel?!" // Ganti sesuka luh
+global.fakeUrl = 'https://chels.my.id'// Ganti sesuka luh
+global.TextT = `Chel AI` // Ganti sesuka luh
+global.botName = "Chel AI"// Ganti sesuka luh
+global.sessionName = "session"
+global.setmenu = "livelocation"
+//global.docType = ""
+global.Qoted = "m"
+global.baileysMd = true
+global.antiSpam = true
+global.fake = Chel AI
+global.autoBio = false
+global.Console = false
+global.publik = true
+global.limitCount = 100 // Ganti sesuka luh
+global.gcounti = { 'prem' : 60, 'user' : 20 } // Ganti sesuka luh
+global.copyright = `© 2024 Im Chel?!` // Ganti sesuka luh
+global.baileysVersion = `Baileys version ${version}ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ`
+global.textT = `Baileys version ${version}ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ`
+global.fake1 = "Chel AI" // Ganti sesuka luh
+global.packName = "Chel AI" // Ganti sesuka luh
+global.authorName = "Created By Chel AI" // Ganti sesuka luh
+global.replyType = "web" // Setting => mess,web,web2,quoted 
+global.setwelcome = "type11"
+global.autoblockcmd = false
+global.autoReport = true
+global.autoLevel = true
+global.multi = true
+global.autoSticker = false
+global.Intervalmsg = 1000 // Detik
+global.app = '•' // Ganti sesuka luh
+global.apz = '⎘' // Ganti sesuka luh
+global.teksMenu = ''
+global.apiflash = '9b9e84dfc18746d4a19d3afe109e9ea4'
+global.Dana = '62895385067778'
+global.syt = 'https://www.youtube.com/@'
+global.sgc = 'https://chat.whatsapp.com/ICGggaBZI8s5CkQ0yEK3K9'
+global.sig = 'https://instagram.com/chel_hbs'
+global.filestackApi ="" //daftar di filestack
+
+global.ehanzUrl = 'https://graph.org/file/bb490fde02bbaafd80c81.jpg'
+global.linkUrl = 'https://wa.me/62877424664779' // Ganti sesuka luh
+
+//===========================================================//
+//APIKEY NYA KALIAN DAFTAR DULU KALO BLOM PUNYA GA ADA APIKEY FITUR GA BERJALAN LANCAR
+global.api = {
+ehz: '', //skizo.tech | kalo mau pake api gua izin dulu
+angel: 'zenzkey_8bb60993ae', // Apikey Zahwazein
+Lol: 'GataDios',
+Botcahx: 'Admin',
+ApikeyAi: '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',
+ApiNobg: 'r1DRrnqgdPT4ceHLUo9m4hTa',
+FilestackApi: '',
+}
+//👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇
+//https://www.skizo.tech [ Api ehz ]
+//https://www.remove.bg/id/api [ ApiNobg ]
+//https://www.filestack.com/ [ FilestackApi ]
+//https://platform.openai.com/account/api-keys [ ApikeyAi ]
+//===========================================================//
+
+
+
+
+
